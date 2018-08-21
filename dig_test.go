c80c4eeae6e5f97d26b1ddccf961e74bb6b230b2
lixz <lixz@dns.com>
2018-08-21 17:26:17 +0800

    add tests

--- dig_test.go
+++ dig_test.go
@@ -15,6 +15,7 @@ func TestDig_SetDNS(t *testing.T) {
 	}{
 		{name: "ipv4", args: args{"114.114.114.114"}, wantErr: false},
 		{name: "ipv4+port", args: args{"114.114.114.114:53"}, wantErr: false},
+		{name: "bad ipv4+port", args: args{"114.114.114.1141:53"}, wantErr: true},
 		{name: "host", args: args{"ns1.dns.com"}, wantErr: false},
 		{name: "host+port", args: args{"ns1.dns.com:53"}, wantErr: false},
 		{name: "ipv6", args: args{"2401::1"}, wantErr: false},
@@ -33,6 +34,28 @@ func TestDig_SetDNS(t *testing.T) {
 			// if _, err := d.A("google.com"); err != nil {
 			// 	t.Errorf("Dig.A(%v) error = %v, wantErr %v", d.RemoteAddr, err, tt.wantErr)
 			// }
+		})
+	}
+}
+
+func TestDig_SetEDNS0ClientSubnet(t *testing.T) {
+
+	tests := []struct {
+		name     string
+		clientip string
+		wantErr  bool
+	}{
+		{name: "", clientip: "", wantErr: true},
+		{name: "bad ipv4", clientip: "1111.2.3.4", wantErr: true},
+		{name: "ipv4", clientip: "1.2.3.4", wantErr: false},
+		{name: "ipv6", clientip: "::1", wantErr: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var dig Dig
+			if err := dig.SetEDNS0ClientSubnet(tt.clientip); (err != nil) != tt.wantErr {
+				t.Errorf("Dig.SetEDNS0ClientSubnet() error = %v, wantErr %v", err, tt.wantErr)
+			}
 		})
 	}
 }
